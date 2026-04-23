4781d26e8330495738d709d9cf79b884b83dd7a3
Shashank Raj <shashankweb111@gmail.com>
2026-04-23 16:04:47 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -21,4 +21,8 @@ const interviewRouter = require("./routes/interview.routes");
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 module.exports = app;

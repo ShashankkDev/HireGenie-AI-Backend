ca890ffc3772aad99f90cc8523b8a27e1ad732d8
Shashank Raj <shashankweb111@gmail.com>
2026-04-24 20:00:38 +0530

    initial commit

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -128,7 +128,7 @@ async function generateInterviewReport({
 
   const response = await retryWithDelay(() =>
     ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-3-flash-preview",
       contents: prompt,
       config: {
         responseMimeType: "application/json",
@@ -212,7 +212,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
 
   const response = await retryWithDelay(() =>
     ai.models.generateContent({
-      model: "gemini-2.0-flash",
+      model: "gemini-3-flash-preview",
       contents: prompt,
       config: {
         responseMimeType: "application/json",

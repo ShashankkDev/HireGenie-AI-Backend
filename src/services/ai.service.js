f61920cd720006d26514f843300ece7371254965
Shashank Raj <shashankweb111@gmail.com>
2026-04-24 17:13:43 +0530

    initial

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -128,7 +128,7 @@ async function generateInterviewReport({
 
   const response = await retryWithDelay(() =>
     ai.models.generateContent({
-      model: "gemini-2.0-flash-lite",
+      model: "gemini-2.0-flash",
       contents: prompt,
       config: {
         responseMimeType: "application/json",
@@ -212,7 +212,7 @@ async function generateResumePdf({ resume, selfDescription, jobDescription }) {
 
   const response = await retryWithDelay(() =>
     ai.models.generateContent({
-      model: "gemini-2.0-flash-lite",
+      model: "gemini-2.0-flash",
       contents: prompt,
       config: {
         responseMimeType: "application/json",

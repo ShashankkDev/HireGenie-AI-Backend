6662fc535e0116b52f78a80491525e33ffdd0dd7
Shashank Raj <shashankweb111@gmail.com>
2026-04-25 12:33:38 +0530

    initial commit

--- src/controllers/interview.controller.js
+++ src/controllers/interview.controller.js
@@ -4,31 +4,37 @@ const {
   generateResumePdf,
 } = require("../services/ai.service");
 const interviewReportModel = require("../models/interviewReport.model");
-const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
+const pdfParse = require("pdf-parse");
+// const pdfjsLib = require("pdfjs-dist/legacy/build/pdf.js");
 
 // fix for vercel
-pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.js");
+// pdfjsLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.js");
 
 /**
  * PDF text extractor (Vercel compatible)
  */
+// async function extractTextFromPDF(buffer) {
+//   const loadingTask = pdfjsLib.getDocument({
+//     data: new Uint8Array(buffer),
+//   });
+
+//   const pdf = await loadingTask.promise;
+
+//   let text = "";
+
+//   for (let i = 1; i <= pdf.numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const content = await page.getTextContent();
+
+//     text += content.items.map((item) => item.str).join(" ") + "\n";
+//   }
+
+//   return text;
+// }
+
 async function extractTextFromPDF(buffer) {
-  const loadingTask = pdfjsLib.getDocument({
-    data: new Uint8Array(buffer),
-  });
-
-  const pdf = await loadingTask.promise;
-
-  let text = "";
-
-  for (let i = 1; i <= pdf.numPages; i++) {
-    const page = await pdf.getPage(i);
-    const content = await page.getTextContent();
-
-    text += content.items.map((item) => item.str).join(" ") + "\n";
-  }
-
-  return text;
+  const data = await pdfParse(buffer);
+  return data.text;
 }
 
 /**
